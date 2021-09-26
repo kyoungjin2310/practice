a589d38c135dba4df33449eef7976f28821d8c3f
kyoungjin2310 <kyoungjin2310@gmail.com>
2021-09-26 22:43:29 +0900

    fix: combobox 수정

--- webToon/src/components/WebToonListFilter.tsx
+++ webToon/src/components/WebToonListFilter.tsx
@@ -8,6 +8,7 @@ import React, {
 import { List, menuTab, webToonList } from "../api/data";
 import { useHistory, useLocation } from "react-router-dom";
 import reducer from "../modules/day/reducer";
+import qs from "qs";
 type WebToonListFilterProps = {
   day: string;
 };
@@ -19,72 +20,69 @@ function getKeyByValue(object: any, value: any) {
 
 function WebToonListFilter({ day }: WebToonListFilterProps) {
   const [state, dispatch] = useReducer(reducer, webToonList);
-  const { favoriteNum, views, fViews, mViews, date }: any = state[0];
   const history = useHistory();
   const [value, setVaule] = useState("");
   const location = useLocation();
   const select = location.pathname.split("/");
-  const home = window.location.origin + "/";
+  // const home = window.location.origin + "/";
 
   //쿼리스트링으로 작성
-  const changeHistory = (key: string) => {
+  const changeHistory = (key: any) => {
     history.push(`/day/week=${day[1]}/${key}`);
   };
 
-  const changeTarget = (key: any) => {
-    dispatch({
-      type: "LIST_SORT",
-      payload: key,
-    });
-    changeHistory(key);
-  };
-
+  //swich 문
   const onChange = useCallback(
     (e: ChangeEvent<HTMLSelectElement>) => {
       setVaule(e.target.value);
       console.log(e.target.value);
+      if (value === "인기순") {
+        const getKey = getKeyByValue(state[0], state[0].favoriteNum);
+        changeHistory(getKey);
+      } else if (value === "여성 인기순") {
+        const getKey = getKeyByValue(state[0], state[0].fViews);
+        changeHistory(getKey);
+      } else if (value === "남성 인기순") {
+        const getKey = getKeyByValue(state[0], state[0].mViews);
+        changeHistory(getKey);
+      } else if (value === "조회순") {
+        const getKey = getKeyByValue(state[0], state[0].views);
+        changeHistory(getKey);
+      } else if (value === "업데이트순") {
+        const getKey = getKeyByValue(state[0], state[0].date);
+        changeHistory(getKey);
+      } else {
+        return;
+      }
     },
     [value]
   );
 
-  // redux빼고 switch 함수문 쓰기
-  {
-    /*function filterWebtoons(type) {
-    switch(type) {
-       case "a": { return a sort }
-    }
-    }*/
-  }
-
   useEffect(() => {
-    console.log(fViews, "fViews");
-    /*if (window.location.href === home) {
-      setVaule("인기순");
-    }*/
-    if (select.length > 3) {
-      console.log(select);
-      console.log(select.length);
-      setVaule(select[select.length - 1]);
-    }
-
-    if (value === "인기순") {
-      const getKey = getKeyByValue(state[0], state[0].favoriteNum);
-      changeTarget(getKey);
-    } else if (value === "여성 인기순") {
-      const getKey = getKeyByValue(state[0], state[0].fViews);
-      changeTarget(getKey);
-    } else if (value === "남성 인기순") {
-      const getKey = getKeyByValue(state[0], state[0].mViews);
-      changeTarget(getKey);
-    } else if (value === "조회순") {
-      const getKey = getKeyByValue(state[0], state[0].views);
-      changeTarget(getKey);
-    } else if (value === "업데이트순") {
-      const getKey = getKeyByValue(state[0], state[0].date);
-      changeTarget(getKey);
-    } else {
-      return;
-    }
+    console.log(location.pathname);
+    // if (select.length > 3) {
+    //   console.log(select);
+    //   console.log(select.length);
+    //   setVaule(select[select.length - 1]);
+    // }
+    // if (value === "인기순") {
+    //   const getKey = getKeyByValue(state[0], state[0].favoriteNum);
+    //   changeHistory(getKey);
+    // } else if (value === "여성 인기순") {
+    //   const getKey = getKeyByValue(state[0], state[0].fViews);
+    //   changeHistory(getKey);
+    // } else if (value === "남성 인기순") {
+    //   const getKey = getKeyByValue(state[0], state[0].mViews);
+    //   changeHistory(getKey);
+    // } else if (value === "조회순") {
+    //   const getKey = getKeyByValue(state[0], state[0].views);
+    //   changeHistory(getKey);
+    // } else if (value === "업데이트순") {
+    //   const getKey = getKeyByValue(state[0], state[0].date);
+    //   changeHistory(getKey);
+    // } else {
+    //   return;
+    // }
   }, [value]);
 
   return (
