dcb731ab78dd28930f05a8b0b444d99080414420
kyoungjin2310 <kyoungjin2310@gmail.com>
2021-11-07 23:52:28 +0900

    feat: context api 이어서 추가

--- ts-react-tutorial/src/contexts/OrderContext.tsx
+++ ts-react-tutorial/src/contexts/OrderContext.tsx
@@ -1,3 +1,16 @@
-import React, { createContext } from "react";
+import React, { createContext, useMemo, useState } from "react";
 
 export const OrderContext = createContext();
+
+export const OrderContextProvider = (props: any) => {
+  const [orderCounts, setOrderCounts] = useState({
+    products: new Map(),
+    options: new Map(),
+  });
+
+  const value = useMemo(() => {
+    return [{ ...orderCounts }];
+  }, [orderCounts]);
+
+  return <OrderContext.Provider value={value}></OrderContext.Provider>;
+};
