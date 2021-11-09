5f9d637a35d976ce97ab4b424daaf616ac40e846
kyoungjin2310 <kyoungjin2310@gmail.com>
2021-11-09 21:43:42 +0900

    파일 삭제

--- ts-react-tutorial/src/mocks/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export const url = process.env.REACT_APP_PUBLIC_URL;
-export const client = axios.create({
-  baseURL: process.env.REACT_APP_BASIC_SERVER_URL,
-});
--- ts-react-tutorial/src/pages/CompletePage/CompletePage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CompletePage = () => {
-  return <div></div>;
-};
-
-export default CompletePage;
