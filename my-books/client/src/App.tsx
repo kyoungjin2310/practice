7909827d02c81486153d20b77943d0697e8928d0
kyoungjin2310 <kyoungjin2310@gmail.com>
2021-11-26 23:49:50 +0900

    fix: login test 추가

--- my-books/client/src/App.tsx
+++ my-books/client/src/App.tsx
@@ -1,37 +1,60 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React, { useState } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import Add from "./pages/Add";
 import Detail from "./pages/Detail";
 import Edit from "./pages/Edit";
 import Home from "./pages/Home";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import NotFound from "./pages/NotFound";
 import SignIn from "./pages/SignIn";
 import Error from "./pages/Error";
 import { ConnectedRouter } from "connected-react-router";
 import history from "./history";
 
+//function App() {
+//return (
+//<ErrorBoundary FallbackComponent={Error}>
+//<ConnectedRouter history={history}>
+//{/*에러가 발생했을때 FallbackComponent로 연결 시켜줌*/}
+//<Switch>
+//{/* 책 정보 편집 */}
+//<Route exact path="/edit/:id" component={Edit}></Route>
+// {/* 책 상세보기 */}
+//          {/* book에 id값 넣어서 조회 */}
+//        <Route exact path="/book/:id" component={Detail}></Route>
+//      {/* 책을 추가 하는 페이지 */}
+//    <Route exact path="/add" component={Add}></Route>
+//  {/* 로그인 */}
+//<Route exact path="/signin" component={SignIn}></Route>
+//<Route exact path="/" component={Home}></Route>
+//{/* 페이지가 없는 경우 - 컴포넌트만 작성*/}
+//<Route component={NotFound}></Route>
+//</Switch>
+//</ConnectedRouter>
+//</ErrorBoundary>
+//);
+//}
+
 function App() {
+  const [logoutUser, setLogoutUser] = useState(false);
   return (
-    <ErrorBoundary FallbackComponent={Error}>
-      <ConnectedRouter history={history}>
-        {/*에러가 발생했을때 FallbackComponent로 연결 시켜줌*/}
+    <BrowserRouter>
+      <div className="App">
+        <h2>JWT Authentication using JSON fake server</h2>
         <Switch>
-          {/* 책 정보 편집 */}
-          <Route exact path="/edit/:id" component={Edit}></Route>
-          {/* 책 상세보기 */}
-          {/* book에 id값 넣어서 조회 */}
-          <Route exact path="/book/:id" component={Detail}></Route>
-          {/* 책을 추가 하는 페이지 */}
-          <Route exact path="/add" component={Add}></Route>
-          {/* 로그인 */}
-          <Route exact path="/signin" component={SignIn}></Route>
-          <Route exact path="/" component={Home}></Route>
-          {/* 페이지가 없는 경우 - 컴포넌트만 작성*/}
-          <Route component={NotFound}></Route>
+          <Route exact path="/">
+            <Header logoutUser={logoutUser} setLogoutUser={setLogoutUser} />
+            <Home logoutUser={logoutUser} />
+          </Route>
+          <Route path="/login">
+            <Login setLogoutUser={setLogoutUser} />
+          </Route>
+          <Route path="/register">
+            <Register setLogoutUser={setLogoutUser} />
+          </Route>
         </Switch>
-      </ConnectedRouter>
-    </ErrorBoundary>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- my-books/client/src/services/TokenService.ts
+++ my-books/client/src/services/TokenService.ts
@@ -1,11 +1,11 @@
-const LOCAL_STORAGE_TOKEN_KEY_NAME = "token";
+const LOCAL_STORAGE_TOKEN_KEY_NAME = "access_token";
 
 export default class TokenService {
   public static get(): string | null {
     return localStorage.getItem(LOCAL_STORAGE_TOKEN_KEY_NAME);
   }
-  public static set(token: string): void {
-    localStorage.setItem(LOCAL_STORAGE_TOKEN_KEY_NAME, token);
+  public static set(access_token: string): void {
+    localStorage.setItem(LOCAL_STORAGE_TOKEN_KEY_NAME, access_token);
   }
   public static remove(): void {
     localStorage.removeItem(LOCAL_STORAGE_TOKEN_KEY_NAME);
