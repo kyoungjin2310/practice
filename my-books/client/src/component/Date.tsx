1601030053734057089edb88bde060e53f4a50fb
kyoungjin2310 <kyoungjin2310@gmail.com>
2021-11-30 21:54:14 +0900

    feat: 요일 구하기 추가

--- my-books/client/src/component/Date.tsx
+++ my-books/client/src/component/Date.tsx
@@ -1,9 +1,25 @@
-import React from "react";
-import moment from "moment";
+import React, { useState, useRef, ChangeEvent } from "react";
+import moment from "moment-timezone";
+import "moment/locale/ko";
 
 const Date = () => {
-  //한국어로 표기
-  return <div>{moment("07-17-2021").format("YYYY년 M월 D일")}</div>;
+  const [day, setDay] = useState("");
+  let birthDayRef = useRef(null);
+  const hendlerBirthDayChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setDay(moment(e.target.value, "YYYY-MM-DD").format("dddd"));
+  };
+  return (
+    <>
+      {/* 한국어로 표기 */}
+      <div>{moment("07-17-2021").format("YYYY년 M월 D일")}</div>;
+      {/* 요일 구하기 */}
+      <div>
+        <input type="date" ref={birthDayRef} onChange={hendlerBirthDayChange} />
+        <h2>요일 구하기</h2>
+        <p>{day}</p>
+      </div>
+    </>
+  );
 };
 
 export default Date;
