0d049e83eb847d0a076d74a3d616d74a690bc3f5
kyoungjin2310 <kyoungjin2310@gmail.com>
2021-12-03 23:36:57 +0900

    feat: 책 login test

--- my-books/client/src/App.tsx
+++ my-books/client/src/App.tsx
@@ -14,51 +14,51 @@ import Header from "./component/Header";
 import Login from "./component/Login";
 import Register from "./component/Register";
 
-//function App() {
-//return (
-//<ErrorBoundary FallbackComponent={Error}>
-//<ConnectedRouter history={history}>
-//{/*에러가 발생했을때 FallbackComponent로 연결 시켜줌*/}
-//<Switch>
-//{/* 책 정보 편집 */}
-//<Route exact path="/edit/:id" component={Edit}></Route>
-// {/* 책 상세보기 */}
-//          {/* book에 id값 넣어서 조회 */}
-//        <Route exact path="/book/:id" component={Detail}></Route>
-//      {/* 책을 추가 하는 페이지 */}
-//    <Route exact path="/add" component={Add}></Route>
-//  {/* 로그인 */}
-//<Route exact path="/signin" component={SignIn}></Route>
-//<Route exact path="/" component={Home}></Route>
-//{/* 페이지가 없는 경우 - 컴포넌트만 작성*/}
-//<Route component={NotFound}></Route>
-//</Switch>
-//</ConnectedRouter>
-//</ErrorBoundary>
-//);
-//}
-
 function App() {
-  const [logoutUser, setLogoutUser] = useState(false);
   return (
-    <BrowserRouter>
-      <div className="App">
-        <h2>JWT Authentication using JSON fake server</h2>
+    <ErrorBoundary FallbackComponent={Error}>
+      <ConnectedRouter history={history}>
+        {/*에러가 발생했을때 FallbackComponent로 연결 시켜줌*/}
         <Switch>
-          <Route exact path="/">
-            <Header logoutUser={logoutUser} setLogoutUser={setLogoutUser} />
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Login setLogoutUser={setLogoutUser} />
-          </Route>
-          <Route path="/register">
-            <Register setLogoutUser={setLogoutUser} />
-          </Route>
+          {/* 책 정보 편집 */}
+          <Route exact path="/edit/:id" component={Edit}></Route>
+          {/* 책 상세보기 */}
+          {/* book에 id값 넣어서 조회 */}
+          <Route exact path="/book/:id" component={Detail}></Route>
+          {/* 책을 추가 하는 페이지 */}
+          <Route exact path="/add" component={Add}></Route>
+          {/* 로그인 */}
+          <Route exact path="/signin" component={SignIn}></Route>
+          <Route exact path="/" component={Home}></Route>
+          {/* 페이지가 없는 경우 - 컴포넌트만 작성*/}
+          <Route component={NotFound}></Route>
         </Switch>
-      </div>
-    </BrowserRouter>
+      </ConnectedRouter>
+    </ErrorBoundary>
   );
 }
+
+//function App() {
+//  const [logoutUser, setLogoutUser] = useState(false);
+//  return (
+//    <BrowserRouter>
+//      <div className="App">
+//        <h2>JWT Authentication using JSON fake server</h2>
+//        <Switch>
+//          <Route exact path="/">
+//            <Header logoutUser={logoutUser} setLogoutUser={setLogoutUser} />
+//            <Home />
+//          </Route>
+//          <Route path="/login">
+//            <Login setLogoutUser={setLogoutUser} />
+//          </Route>
+//          <Route path="/register">
+//            <Register setLogoutUser={setLogoutUser} />
+//          </Route>
+//        </Switch>
+//      </div>
+//   </BrowserRouter>
+// );
+//}
 
 export default App;
--- my-books/client/src/services/UserService.ts
+++ my-books/client/src/services/UserService.ts
@@ -10,13 +10,13 @@ const USER_API_URL = process.env.SERVER_URL;
 export default class UserService {
   public static async login(reqData: LoginReqType): Promise<string> {
     const response = await axios.post(
-      `http://localhost:4000/api/auth/login`,
+      `http://localhost:5000/api/auth/login`,
       reqData
     );
     return response.data.access_token;
   }
   public static async logout(access_token: string): Promise<void> {
-    await axios.delete(`http://localhost:4000/api/auth/login`, {
+    await axios.delete(`http://localhost:5000/api/auth/login`, {
       headers: { Authorization: `Bearer ${access_token}` },
     });
   }
