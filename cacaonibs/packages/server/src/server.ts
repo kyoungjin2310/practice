58960c16e0e1e520e811dbb1f14a2757d9358543
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-02-02 23:36:31 +0900

    feat: session 설정

--- cacaonibs/packages/server/src/server.ts
+++ cacaonibs/packages/server/src/server.ts
@@ -2,8 +2,20 @@ import express, { Application } from "express";
 import sequelize from "./sequelize";
 import session from "express-session";
 import routes from "./routes";
+
+const FileStore = require("session-file-store")(session);
+
 const app: Application = express();
 
+const sessionMiddleware = session({
+  secret: "cacaonibs",
+  saveUninitialized: true,
+  cookie: { secure: false },
+  resave: false,
+  store: new FileStore(),
+});
+
+app.use(sessionMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 sequelize.sync({ force: true });
