d4ff2164a021bf3ec77a9680fc010f89186f2cf2
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-02-08 23:46:48 +0900

    feat: 컴포넌트 추가

--- cacaonibs/packages/client/src/App.tsx
+++ cacaonibs/packages/client/src/App.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Friends from "./pages/Friends";
+import Lobby from "./pages/Lobby";
+import RoomDetail from "./pages/RoomDetail";
+import RoomList from "./pages/RoomList";
+import SeeMore from "./pages/SeeMore";
 
 function App() {
   return (
     <Routes>
       {/*main*/}
-      <Route index element={} />
+      <Route index element={Lobby} />
       {/*친구목록*/}
-      <Route path="/" element={} />
+      <Route path="/" element={Friends} />
       {/*대화방*/}
-      <Route index element={} />
+      <Route index element={RoomList} />
       {/*대화방상세*/}
-      <Route index element={} />
+      <Route index element={RoomDetail} />
       {/*더보기*/}
-      <Route index element={} />
+      <Route index element={SeeMore} />
     </Routes>
   );
 }
