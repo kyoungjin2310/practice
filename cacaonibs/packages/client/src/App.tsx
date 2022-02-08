582227c40a441260d4eabb7e682e82661df5d96a
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-02-08 23:45:11 +0900

    feat: react router 추가

--- cacaonibs/packages/client/src/App.tsx
+++ cacaonibs/packages/client/src/App.tsx
@@ -1,8 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      {/*main*/}
+      <Route index element={} />
+      {/*친구목록*/}
+      <Route path="/" element={} />
+      {/*대화방*/}
+      <Route index element={} />
+      {/*대화방상세*/}
+      <Route index element={} />
+      {/*더보기*/}
+      <Route index element={} />
+    </Routes>
+  );
 }
 
 export default App;
