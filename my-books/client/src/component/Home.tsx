3da8aa5bac70b62aabf027a659553ecbd7b00e47
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-04-19 23:23:10 +0900

    fix: server 수정

--- my-books/client/src/component/Home.tsx
+++ my-books/client/src/component/Home.tsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   const isLoginTrue = JSON.parse(localStorage.getItem("login") || "{}");
-
   const userNotLogin = () => (
     <>
       <h2>It seem's like you are not login</h2>
