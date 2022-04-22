1a5bb7857fa634779869bfb58f7ff1a52c0380fc
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-04-22 23:34:04 +0900

    fix: list component 수정

--- my-books/client/src/component/List.tsx
+++ my-books/client/src/component/List.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { Button, PageHeader } from "antd";
 import { BookType } from "../type";
 import Layout from "./Layout";
 
@@ -18,16 +19,14 @@ const List: React.FC<ListProps> = ({ books, loading, getBooks }) => {
 
   return (
     <Layout>
-      <div>Book List</div>
-      <button onClick={goAdd}>Add Book</button>
-      <button onClick={logout}>Logout</button>
-      <div>
-        <ul>
-          <li>
-            <h2>Book</h2>
-          </li>
-        </ul>
-      </div>
+      <PageHeader
+        title={<div>Book List</div>}
+        extra={[
+          <Button key="2" type="primary" onClick={goAdd}>
+            Add Book
+          </Button>,
+        ]}
+      ></PageHeader>
     </Layout>
   );
 };
