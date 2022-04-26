dabacc5d83286857df57043acc774acf78441d68
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-04-26 22:47:35 +0900

    feat: List css 추가

--- my-books/client/src/component/List.tsx
+++ my-books/client/src/component/List.tsx
@@ -3,30 +3,54 @@ import { Button, PageHeader, Table } from "antd";
 import { BookType } from "../type";
 import Layout from "./Layout";
 import Book from "./Book";
+import styles from "./List.module.css";
 
 type ListProps = {
   books: BookType[] | null;
   loading: boolean;
+  error: Error | null;
   getBooks: () => void;
+  logout: () => void;
 };
 
-const List: React.FC<ListProps> = ({ books, loading, getBooks }) => {
-  const logout = () => {};
+const List: React.FC<ListProps> = ({
+  books,
+  loading,
+  getBooks,
+  error,
+  logout,
+}) => {
   const goAdd = () => {};
 
   useEffect(() => {
     getBooks();
   }, [getBooks]);
 
+  useEffect(() => {
+    if (error) {
+      logout();
+    }
+  }, [error, logout]);
+
   return (
     <Layout>
       <PageHeader
         title={<div>Book List</div>}
         extra={[
-          <Button key="2" type="primary" onClick={goAdd}>
+          <Button
+            key="2"
+            type="primary"
+            onClick={goAdd}
+            className={styles.button}
+          >
             Add Book
           </Button>,
-          <Button key="1" type="primary" onClick={logout}>
+          <Button
+            key="1"
+            type="primary"
+            onClick={logout}
+            className={styles.button}
+          >
             Logout
           </Button>,
         ]}
@@ -45,6 +69,7 @@ const List: React.FC<ListProps> = ({ books, loading, getBooks }) => {
         showHeader={false}
         rowKey="bookId"
         pagination={false}
+        className={styles.table}
       ></Table>
     </Layout>
   );
