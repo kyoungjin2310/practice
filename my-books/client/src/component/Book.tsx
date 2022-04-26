adbdc2f785189dd25a1f0db2a7894aca7253913b
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-04-26 22:40:27 +0900

    feat: book css 추가

--- my-books/client/src/component/Book.tsx
+++ my-books/client/src/component/Book.tsx
@@ -9,6 +9,7 @@ import {
 import { BookType } from "../type";
 import moment from "moment";
 import { Button, Tooltip } from "antd";
+import styles from "./Book.module.css";
 
 interface BookProps extends BookType {}
 
@@ -20,29 +21,44 @@ const Book: React.FC<BookProps> = ({
   url,
 }) => {
   return (
-    <div>
-      <div>
-        <Link to={`/book/${bookId}`}>
+    <div className={styles.book}>
+      <div className={styles.title}>
+        <Link to={`/book/${bookId}`} className={styles.link_detail_title}>
           <BookOutlined>{title}</BookOutlined>
         </Link>
       </div>
-      <div>
-        <Link to={`/book/${bookId}`}>{author}</Link>
+      <div className={styles.author}>
+        <Link to={`/book/${bookId}`} className={styles.link_detail_author}>
+          {author}
+        </Link>
       </div>
-      <div>{moment(createdAt).format("MM-DD-YYYY hh:mm a")}</div>
-      <div>
+      <div className={styles.created}>
+        {moment(createdAt).format("MM-DD-YYYY hh:mm a")}
+      </div>
+      <div className={styles.tooltips}>
         <Tooltip title={url}>
-          <a href={url} rel="noreferrer" target="_blank">
+          <a
+            href={url}
+            rel="noreferrer"
+            target="_blank"
+            className={styles.link_url}
+          >
             <Button
               size="small"
               type="primary"
               shape="circle"
               icon={<HomeOutlined />}
+              className={styles.button_url}
             ></Button>
           </a>
         </Tooltip>
         <Tooltip title="Edit">
-          <Button size="small" shape="circle" icon={<EditOutlined />}></Button>
+          <Button
+            size="small"
+            shape="circle"
+            icon={<EditOutlined />}
+            className={styles.button_edit}
+          ></Button>
         </Tooltip>
         <Tooltip title="Delete">
           <Button
@@ -51,6 +67,7 @@ const Book: React.FC<BookProps> = ({
             shape="circle"
             danger
             icon={<DeleteOutlined />}
+            className={styles.button_delete}
           ></Button>
         </Tooltip>
       </div>
