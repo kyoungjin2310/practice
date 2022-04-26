8a10ec7b02280c81c73b93c290fcc53ee652744d
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-04-26 22:50:54 +0900

    feat: logout 추가

--- my-books/client/src/containers/ListContainer.tsx
+++ my-books/client/src/containers/ListContainer.tsx
@@ -3,12 +3,17 @@ import { useDispatch, useSelector } from "react-redux";
 import List from "../component/List";
 import { BookType, RootState } from "../type";
 import { getBooks as getBooksSagaStart } from "../redux/modules/books";
+import { logout as logoutSagaStart } from "../redux/modules/auth";
+
 const ListContainer = () => {
   const books = useSelector<RootState, BookType[] | null>(
     (state) => state.books.books
   );
   const loading = useSelector<RootState, boolean>(
     (state) => state.books.loading
+  );
+  const error = useSelector<RootState, Error | null>(
+    (state) => state.books.error
   );
 
   const dispatch = useDispatch();
@@ -17,7 +22,19 @@ const ListContainer = () => {
     dispatch(getBooksSagaStart());
   }, [dispatch]);
 
-  return <List books={books} loading={loading} getBooks={getBooks} />;
+  const logout = useCallback(() => {
+    dispatch(logoutSagaStart);
+  }, [dispatch]);
+
+  return (
+    <List
+      books={books}
+      loading={loading}
+      getBooks={getBooks}
+      error={error}
+      logout={logout}
+    />
+  );
 };
 
 export default ListContainer;
